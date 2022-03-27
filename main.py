f666b40e17c739b95d8052ef18fb051c3194d8ef
NotRage <59664001+notrage@users.noreply.github.com>
2022-03-27 14:51:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tracemalloc import reset_peak
+from turtle import Screen
 import keyboard as k
 import time as tm
 import pyautogui as p
@@ -6,11 +8,13 @@ import tkinter as tk
 
 SCREEN = tk.Tk()
 SCREEN_WIDTH = SCREEN.winfo_screenwidth()
+SCREEN_HEIGHT = SCREEN.winfo_screenheight()
 cobblestone_farming = False
 
 def string_typing(string):
     for letter in string:
-        k.press_and_release(letter)
+        if letter == ' ': k.press_and_release('space')
+        else: k.press_and_release(letter)
         tm.sleep(0.1)
     k.press_and_release('enter')
     
@@ -34,6 +38,19 @@ def reset_farming():
     k.press_and_release('f')
     tm.sleep(2)
     k.press_and_release('f')
+    
+def reset_game():
+    cobblestone_farming = disable_farming()
+    k.press_and_release('alt + F4')
+    k.press_and_release('windows')
+    string_typing('lunar')
+    tm.sleep(15)
+    p.click(SCREEN_WIDTH/2, SCREEN_HEIGHT/4)
+    tm.sleep(30)
+    k.press_and_release('f')
+    p.click(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.75)
+    p.doubleClick(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    string_typing('tplay sb')
 
 print("COBBLESTONE FARMER MACHINE STARTING \n",
       "- Press 'shift' to start farming \n",
@@ -54,14 +71,14 @@ while True:
     
     if k.is_pressed('esc'): cobblestone_farming = disable_farming()
         
-    if (k.is_pressed('r') ) or (
-       (d.datetime.now().minute in [00, 10, 20, 30, 40, 50]) and
-       (d.datetime.now().second == 00)
-                                ): reset_farming()
-    
-    if d.datetime.now().second in [00, 10, 20, 30, 40, 50]: k.press_and_release('space')
-        
     if cobblestone_farming:
+        
+        if (k.is_pressed('r') ) or ((d.datetime.now().minute in [00, 10, 20, 30, 40, 50]) and (d.datetime.now().second == 00)): reset_farming()
+        
+        if d.datetime.now().second in [00, 10, 20, 30, 40, 50]: k.press_and_release('space')
+        
+        if d.datetime.now().hour in [00, 8, 16]: reset_game() 
+        
         k.press('z + k')
         tm.sleep(0.1)
         
